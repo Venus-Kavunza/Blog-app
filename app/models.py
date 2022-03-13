59b436a7c56b77391438265264a9a55279f4c4da
Venus-Kavunza <venusmwende@gmail.com>
2022-03-13 19:16:39 +0300

    It will add blog class to models file

--- app/models.py
+++ app/models.py
@@ -18,3 +18,27 @@ class User(db.Model,UserMixin):
     profile_pic_path = db.Column(db.String())
     blogs = db.relationship('Blog', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Blog(db.Model):
+    __tablename__ = "blogs"
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255))
+    post = db.Column(db.Text())
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comment = db.relationship("Comment",backref="blog",lazy="dynamic")
